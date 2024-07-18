75592ef4ebaf64376531018bfade6a8f67294637
Pornhub12321nah <mrktar30@gmail.com>
2024-07-18 19:24:30 +0800

    Update ai.js

--- modules/commands/ai.js
+++ modules/commands/ai.js
@@ -1,30 +1,66 @@
-const axios = require("axios");
-
+const { getPrefix, getStreamFromURL, uploadImgbb } = global.utils;
+async function ai({ message: m, event: e, args: a, usersData: u }) {
+  var p = [`${await getPrefix(e.threadID)}${this.config.name}`,
+`${this.config.name}`
+/*"ai"
+*you can add more prefix here
+*/
+]; 
+ if (p.some(b => a[0].toLowerCase().startsWith(b))) {
+try {      
+let prompt = "";
+if (e.type === "message_reply" && e.messageReply.attachments && e.messageReply.attachments[0]?.type === "photo") {
+ const b = await uploadImgbb(e.messageReply.attachments[0].url);
+prompt = a.slice(1).join(" ") + ' ' + b.image.url;
+} else {
+ prompt = a.slice(1).join(" ");
+}
+ var __ = [{ id: e.senderID, tag: await u.getName(e.senderID) }];
+ const r = await require("axios").post(`https://test-ai-ihc6.onrender.com/api`, {
+  prompt: prompt,
+ apikey: "GayKey-oWHmMb1t8ASljhpgSSUI",
+  name: __[0]['tag'],
+ id: __[0]['id'],
+ });
+var _ = r.data.result.replace(/{name}/g, __[0]['tag']).replace(/{pn}/g, p[0]);
+ if (r.data.av) {
+ if (Array.isArray(r.data.av)) {
+ const avs = r.data.av.map(url => getStreamFromURL(url));
+ const avss = await Promise.all(avs);
+  m.reply({
+ body: _,
+ mentions: __,
+ attachment: avss
+ });
+ } else {
+ m.reply({
+ body: _,
+ mentions: __,
+attachment: await getStreamFromURL(r.data.av)
+  });
+  }
+  } else {
+m.reply({
+body: _,
+mentions: __
+  });
+  }
+  } catch (error) {
+ m.reply("Error " + error);
+ }
+ }
+}
 module.exports = {
-  config: {
-    name: "ai",
-    version: "1.0.0",
-    description: "AI command",
-    usage: "{pn} [message]",
-    author: "Rui",
-    cooldown: 5,
-    usePrefix: false,
-    role: 0,
-  },
-  async onRun({ fonts, api, message, args }) {
-    const query = args.join(" ");
-
-    if (!query) {
-      message.react("❓")
-      message.reply("❌ | Please provide a query!");
-    } else {
-      const info = await
-message.reply(`🔍 | ${query}`);
-      const response = await axios.get(`https://akhiro-rest-api.onrender.com/api/gpt4?q=${encodeURIComponent(query)}`);
-      api.editMessage(
-        `${fonts.bold("🤖 | AI")}\n━━━━━━━━━━━━━━━\n${response.data.content}`,
-        info.messageID,
-      );
-    }
-  },
+config: {
+ name: "ai",
+aliases: [],
+version: 1.6,
+author: "Jun",
+role: 0,
+ shortDescription: "An AI that can do various tasks",
+ guide: "{pn} <query>",
+ category: "AI"
+ },
+ onStart: function() {},
+ onChat: ai
 };
